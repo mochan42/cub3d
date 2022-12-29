9f329176927418dcd027f92e9c27c709482a2b87
Facinet Kouyate <fakouyat@student.42wolfsburg.de>
2022-12-29 23:48:29 +0100

    Parsing of the map almost done, next step fill texture and colors

--- src/utils_1.c
+++ src/utils_1.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   utils_1.C                                          :+:      :+:    :+:   */
+/*   utils_1.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: moninechan <moninechan@student.42.fr>      +#+  +:+       +#+        */
+/*   By: fakouyat <fakouyat@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/10 16:31:50 by moninechan        #+#    #+#             */
-/*   Updated: 2022/12/28 23:17:28 by moninechan       ###   ########.fr       */
+/*   Updated: 2022/12/29 23:17:38 by fakouyat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -94,39 +94,162 @@ void    check_direction(t_prg *v, int *validator)
     }
 }
 
+int	isAllowedChar(char c) 
+{
+	if (c == 'N' || c == 'S' || c == 'W' || c == 'E' || c == '1' || c == '0')
+		return (1);
+	return (0);
+}
+
+int	isInitPlayerPos(char c)
+{
+	if (c == 'N' || c == 'S' || c == 'W' || c == 'E')
+		return (1);
+	return (0);
+}
+
+int	isAllowedSpaceY(t_prg *v, int i, int j, int sens)
+{
+	while (v->map[i][j]) 
+	{
+		if (sens == 1)
+			j++;
+		else
+			j--;
+		if (isAllowedChar(v->map[i][j]))
+			return 1;
+	}
+	return 0;
+}
+
+int	isAllowedSpaceX(t_prg *v, int i, int j, int sens)
+{
+	while (v->map[i][j] && v->map[i][j] == 32) 
+	{
+		if (sens == 1)
+		{
+			i++;
+			printf("CONTINUE DOWN\n");
+			
+		}
+		else
+		{
+			i--;
+			printf("CONTINUE TOP\n");
+		}
+		printf("CHAR MEET:%c*\n", v->map[i][j]);
+		if (isAllowedChar(v->map[i][j]))
+			return 1;
+	}
+	return 0;
+}
+
 void    parsing(t_prg *v, int *validator)
 {
     int    p_len;
     int    i;
     int    j;
-
+	int		isPos = 0;
     p_len = ft_strlen(v->map_path);
     if (ft_strncmp(&v->map_path[p_len - 4], ".cub", 4) != 0)    
     {
         printf("Invalid map : The file extension should be .cub \n");
         *validator = 1;
+		return ;
     }
+	printf("MAP CURSOR : %d\n", v->map_i);
     skipe_empty_line(v);
+	printf("MAP CURSOR : %d\n", v->map_i);
     check_direction(v, validator);
+	printf("MAP CURSOR AFTER CHECK DIRECTION: %d\n", v->map_i);
     skipe_empty_line(v);
-    i = 0;
+	printf("MAP CURSOR BEFORE MAP: %d\n", v->map_i);
+	
+    i = v->map_i;
     j = 0;
-    // while (v->map[v->map_i][i] != '\0' || v->map[v->row - 1][j] != '\0')
-    // {
-    //     if ((v->map[v->map_i][i] != '1' && v->map[v->map_i][i] != '\n')
-    //         && (v->map[v->map_i][i] != 32 && v->map[v->map_i][i] != '\0'))
-    //         *validator = 1;
-    //     if ((v->map[v->row - 1][j] != '1')
-    //         && (v->map[v->row - 1][j] != 32 && v->map[v->row -1][j] != '\0'))
-    //         *validator = 1;
-    //     if (*validator == 1)
-    //     {
-    //         printf("The map should be surrounded by wall : 1\n");
-    //         break ;
-    //     }
-    //     i++;
-    //     j++;
-    // }
-    printf("First line map :%s", v->map[v->map_i]);
-    printf("Last line map :%s\n", v->map[v->row - 1]);
+	while (i < v->row)
+	{
+		j = 0;
+		while (v->map[i][j]) {
+			if (v->map[i][0] == '\0')
+			{
+				printf("Invalid map\n");
+				*validator = 1;
+				return ;
+			}
+			if (!isAllowedChar(v->map[i][j]) && v->map[i][j] != 32 && v->map[i][j] != '\n')
+			{
+				printf("Invalid character in map\n");
+				*validator = 1;
+				return ;
+			}
+			if (v->map[i][j] == '0' || isInitPlayerPos(v->map[i][j]))
+			{
+				if (isInitPlayerPos(v->map[i][j]))
+					isPos++;
+				if (i == v->map_i || i == v->row - 1 || j == 0)
+				{
+					printf("The map should be surronded by the wall\n");
+					*validator = 1;
+					return ;
+				}
+				if (!isAllowedChar(v->map[i - 1][j]))
+				{
+					if (isAllowedSpaceX(v, i - 1, j, 0))
+						;
+					else
+					{
+						printf("The map should be surronded by the wall\n");
+						*validator = 1;
+						return ;
+					}
+				}
+				if (!isAllowedChar(v->map[i + 1][j]))
+				{
+					if (isAllowedSpaceX(v, i + 1, j, 1))
+						;
+					else
+					{
+						printf("The map should be surronded by the wall\n");
+						*validator = 1;
+						return ;
+					}
+				}
+				if (v->map[i][j + 1] && !isAllowedChar(v->map[i][j + 1]))
+				{
+					if (isAllowedSpaceY(v, i, j + 1, 1))
+						;
+					else
+					{
+						printf("The map should be surronded by the wall\n");
+						*validator = 1;
+						return ;
+					}
+				}
+				if (!isAllowedChar(v->map[i][j - 1]))
+				{
+					if (isAllowedSpaceY(v, i, j - 1, 0))
+						;
+					else
+					{
+						printf("The map should be surronded by the wall\n");
+						*validator = 1;
+						return ;
+					}
+				}
+			}
+			j++;
+		}
+		i++;
+	}
+	if (!isPos)
+	{
+		printf("The map should have a player (N, S, W, E)\n");
+		*validator = 1;
+	}
+	else if (isPos > 1)
+	{
+		*validator = 1;
+		printf("The map should have only one player position\n");
+	}
 }
